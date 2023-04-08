0a64a4160aeb77e89a419da00decbbc3573247ac
Steve Esteban <lombardo.e@hotmail.com>
2023-04-08 18:51:04 -0300

    Add signal of object

--- angular-signals/src/main.ts
+++ angular-signals/src/main.ts
@@ -1,5 +1,5 @@
 import 'zone.js/dist/zone';
-import { Component, effect, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -18,6 +18,10 @@ import { FormsModule } from '@angular/forms';
     <option disabled value="">--Select a quantity--</option>
     <option *ngFor="let q of qtyAvailable()"> {{ q }}</option>
     </select>
+
+    <div>Vehicle: {{ selectedVehicle().name}}</div>
+    <div>Price: {{ selectedVehicle().price | number: '1.2-2'}}</div>
+    <div style="font-weight: bold">Total: {{ exPrice()  | number: '1.2-2' }}</div>
   `,
 })
 export class App {
@@ -25,16 +29,28 @@ export class App {
   quantity = signal(1);
   qtyAvailable = signal([1, 2, 3, 4, 5, 6]);
 
+  selectedVehicle = signal<Vehicle>({id: 1, name: "car 1", price: 1000});
+
+  vehicles = signal<Vehicle[]>([]);
+
+  // Computed signal
+  exPrice = computed(() => this.selectedVehicle().price * this.quantity());
+
   constructor() {
     console.log(this.quantity());
 
+    // Update signal
     this.quantity.update(qty => qty * 2);
+
+    // Mutate signal
+    this.selectedVehicle.mutate((v) => v.price = v.price * 2);
   }
 
   // We usually use effects for debugging purposes
   qtyEff = effect(() => console.log(this.quantity()));
 
   onQuantitySelected(qty: number) {
+    // Set signal
     this.quantity.set(qty);
 
     // If this was uncommented, it would take always the latest set value, ignoring the intermidiate ones
@@ -44,3 +60,9 @@ export class App {
 }
 
 bootstrapApplication(App);
+
+export interface Vehicle {
+  id: number;
+  name: string;
+  price: number;
+}
